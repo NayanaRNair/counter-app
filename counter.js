3e3375f621f64975da79bda9c0233efa86422c4f
Nayana Rajeev <84270364+NayanaRNair@users.noreply.github.com>
2022-01-25 20:43:03 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,10 @@
+function add(){
+    let val=document.getElementById("num").innerHTML
+    let num=Number(val)+1
+    document.getElementById("num").innerHTML=num
+}
+function sub(){
+    let val=document.getElementById("num").innerHTML
+    let num=Number(val)-1
+    document.getElementById("num").innerHTML=num
+}
